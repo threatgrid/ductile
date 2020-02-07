8b9e73eac17c3c301cfaf50fdc37b7043dee7818
Guillaume ERETEO <gereteo@cisco.com>
2020-02-07 18:06:14 +0100

    es7 compliant

--- test/ductile/index_test.clj
+++ test/ductile/index_test.clj
@@ -183,7 +183,6 @@
             :search_routing "kimchy"}
            (:alias2 aliases)))
     (is (= 2 (count aliases)))
-    (is (= index_patterns ["pattern1" "pattern2"]))
     (is (= {:acknowledged true}
            (sut/create-template! conn
                                  template-name-2
