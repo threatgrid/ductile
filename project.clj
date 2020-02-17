37742d45714d8571b163f8c01e6155315f901a8f
Guillaume ERETEO <gereteo@cisco.com>
2020-02-17 18:08:26 +0100

    clean project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (def clj-version "1.10.1")
-(def metrics-clojure-version "2.10.0")
 
 (defproject threatgrid/ductile "0.1.0-SNAPSHOT"
   :description "Yet another Clojure client for Elasticsearch REST API, that fits our needs"
@@ -12,16 +11,7 @@
                  [metosin/schema-tools "0.12.2"]
                  [clj-http "3.10.0"] ;TODO bump clj-http with https://github.com/dakrone/clj-http/pull/532
                  [com.arohner/uri "0.1.2"]
-                 [cheshire "5.9.0"]
-                 [riemann-clojure-client "0.5.1"]
-                 [metrics-clojure ~metrics-clojure-version]
-                 [metrics-clojure-jvm ~metrics-clojure-version]
-                 [metrics-clojure-ring ~metrics-clojure-version]
-                 [metrics-clojure-riemann ~metrics-clojure-version
-                  :exclusions [;renamed to (and incompatible with) `io.riemann/riemann-java-client`,
-                                        ;which is provided by `riemann-clojure-client`.
-                               com.aphyr/riemann-java-client]]
-                 [clout "2.2.1"]]
+                 [cheshire "5.9.0"]]
   :pedantic? :abort
   :main nil
   :codox {:output-path "doc"
@@ -30,7 +20,8 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies
-                   [[ch.qos.logback/logback-classic "1.2.3"]]}
+                   [[ch.qos.logback/logback-classic "1.2.3"]]
+                   :pedantic? :warn}
              :test {:dependencies
                     [[clj-http-fake "1.0.3"]
                      [ring/ring-codec "1.1.2"]]
