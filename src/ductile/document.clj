2b30bef6ba1c43d8f6c0f935af3cdc7b34d47075
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-02-28 13:55:15 -0500

    dbg

--- src/ductile/document.clj
+++ src/ductile/document.clj
@@ -478,8 +478,8 @@
                                  :inline (string/join
                                            "\n"
                                            ;; https://www.elastic.co/guide/en/elasticsearch/painless/8.1/painless-walkthrough.html#_missing_keys
-                                           [(format "if (!doc.containsKey('%s') || doc['%s'].empty) { Debug.explain(doc); return params.default }"
-                                                    field-name field-name)
+                                           [(format "if (!doc.containsKey('%s') || doc['%s'].empty) { Debug.explain(doc.containsKey('%s')); return params.default }"
+                                                    field-name field-name field-name)
                                             (format "return Debug.explain(params.remappings.getOrDefault(doc['%s'].value, params.default))"
                                                     field-name)])
                                  :params {:remappings remappings
