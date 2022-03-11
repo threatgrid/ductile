c1f5208ffa1441295eea0adbed9ff968b1b19afb
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-03-11 14:30:08 -0500

    trim

--- src/ductile/document.clj
+++ src/ductile/document.clj
@@ -441,63 +441,6 @@
       request-fn
       conn/safe-es-read))
 
-#_
-(defn sort-params-ext
-  [sort_by_ext default-sort_order]
-  (assert (sequential? sort_by_ext))
-  {:sort (mapv (fn [{:keys [op field-name sort_order] :as params}]
-                 (let [field-name (name field-name)
-                       order (keyword (or sort_order default-sort_order))]
-                  (assert (keyword? order) (pr-str order))
-                  (assert (not (some #{"'"} field-name)) (pr-str field-name))
-                  (case op
-                    ;; eg
-                    ;; {:op :field
-                    ;;  :field-name "Severity"
-                    ;;  :sort_order :asc}
-                    :field
-                    ;; https://www.elastic.co/guide/en/elasticsearch/reference/current/sort-search-results.html#_sort_values
-                    {field-name {:order order}}
-
-                    ;; eg
-                    ;; {:op :remap
-                    ;;  :remap-type :number
-                    ;;  :field-name "severity"
-                    ;;  :remappings {"Critical" 0
-                    ;;               "High" 1}
-                    ;;  :sort_order :asc
-                    ;;  :remap-default 0}
-                    :remap
-                    (let [{:keys [remap-type remap-default remappings]} params
-                          remappings (into {} (map (fn [e]
-                                                     (update e 0 #(cond-> %
-                                                                    (string? %) string/lower-case))))
-                                           remappings)]
-                      (assert ((some-fn string? simple-keyword?) remap-type) (str "Expected eg., :remap-type :number, actual " (pr-str remap-type)))
-                      (assert ((every-pred map? seq) remappings) (pr-str remappings))
-                      (assert (some? remap-default) (pr-str remap-default))
-                      (assert (every? string? (keys remappings)) remappings)
-                      (case remap-type
-                        :number (do (assert (number? remap-default) remap-default)
-                                    (assert (every? number? (vals remappings)) remappings))
-                        :string (do (assert (string? remap-default) remap-default)
-                                    (assert (every? string? (vals remappings)) remappings)))
-                      ;; https://www.elastic.co/guide/en/elasticsearch/painless/current/painless-sort-context.html
-                      {:_script
-                       {:type (name remap-type)
-                        :script {:lang "painless"
-                                 :inline (string/join
-                                           "\n"
-                                           ;; https://www.elastic.co/guide/en/elasticsearch/painless/8.1/painless-walkthrough.html#_missing_keys
-                                           [(format "if (!doc.containsKey('%s') || doc['%s'].size() != 1) { return params.default }"
-                                                    field-name field-name)
-                                            (format "return params.remappings.getOrDefault(doc['%s'].value, params.default)"
-                                                    field-name)])
-                                 :params {:remappings remappings
-                                          :default remap-default}}
-                        :order order}}))))
-               sort_by_ext)})
-
 (defn sort-params
   [sort_by sort_order]
   (let [sort-fields
