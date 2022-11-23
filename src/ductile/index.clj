1a340304397b32ce0f379a9860d8fb927b874a0d
Kirill Chernyshov <kchernys@cisco.com>
2022-11-23 12:00:29 +0100

    Move ductile.settings/fetch into index namespace
    `ductile.settings/fetch` -> `ductile.index/get-settings`

--- src/ductile/index.clj
+++ src/ductile/index.clj
@@ -1,11 +1,14 @@
 (ns ductile.index
   (:refer-clojure :exclude [get])
-  (:require [ductile.uri :as uri]
-            [ductile.conn :refer [make-http-opts safe-es-read]]
-            [ductile.schemas :refer [ESConn RolloverConditions CatIndices]]
-            [schema.core :as s]
-            [schema-tools.core :as st]
-            [clojure.edn :as edn]))
+  (:require
+   [clojure.edn :as edn]
+   [clojure.string :as string]
+   [clojure.walk :as walk]
+   [ductile.conn :refer [make-http-opts safe-es-read]]
+   [ductile.schemas :refer [CatIndices ESConn RolloverConditions ESSettings]]
+   [ductile.uri :as uri]
+   [schema-tools.core :as st]
+   [schema.core :as s]))
 
 (s/defn index-uri :- s/Str
   "make an index uri from a host and an index name"
@@ -61,6 +64,46 @@
              :url (index-uri uri index-name))
       request-fn
       safe-es-read))
+
+(defn coerce [^String st]
+  (try
+    (let [v (edn/read-string st)]
+      (if (symbol? v) st v))
+    (catch Exception _ st)))
+
+(defn deep-merge [m1 m2]
+  (if (and (map? m1) (map? m2))
+    (merge-with deep-merge m1 m2)
+    (or m2 m1)))
+
+(defn merge-defaults [settings]
+  (walk/postwalk
+   (fn [f]
+     (if (string? f)
+       (coerce f)
+       f))
+   (reduce-kv
+    (fn [acc index {:keys [defaults settings]}]
+      (if (string/starts-with? (name index) ".")
+        acc
+        (assoc acc index (deep-merge settings defaults))))
+    {}
+    settings)))
+
+(s/defn get-settings :- ESSettings
+  "Extract settings of an `index` from ES cluster. If explicit `index` is not provided - uses `\"_all\"` as a target.
+   Result is a hash map of `index` -> `settings`, including defaults."
+  ([conn :- ESConn] (get-settings conn "_all"))
+  ([{:keys [uri request-fn] :as conn} :- ESConn
+    index :- s/Str]
+   (-> (make-http-opts conn)
+       (assoc :query-params {:include_defaults true
+                             :ignore_unavailable true}
+              :method :get
+              :url (uri/uri uri (uri/uri-encode index) "_settings"))
+       (request-fn)
+       (safe-es-read)
+       (merge-defaults))))
 
 (s/defn update-settings!
   "update an ES index settings"
